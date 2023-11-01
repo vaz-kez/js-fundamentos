920fef515e1f2dca2b7f65ba47f5c11a9efc5ad6
Marcos Vazquez <marcosvazquez@mzkez.local>
2023-11-01 18:16:30 +0100

    create variables session 01

---
+++ 01-variables.js
@@ -0,0 +1,25 @@
+var greetings = 'hola desde'
+var school = 'pmba'
+
+var greetingsFromSchool = greetings + ' ' + school;
+
+console.log(greetingsFromSchool);
+
+
+// holaQueTal (camel_case) /* mayúscula en lugar de un espacio*/
+// FINAL_TEXT (PASCAL_CASE) /* VARIABLES QUE NO VAN A CAMBIAR NUNCA */
+
+// string  // cadena de texto
+// number  // número
+// boolean // verdadero o falso - encendido o apagado
+// Array  // conjunto valores
+// Object  // librería de valores
+
+var ageAsString = '30' // no utilizar números con esta variable
+var ageAsNumber = 30  // para números
+
+var arrayOfValues = [30, '30', false, true, 4]
+var objectOfValues = {
+  age: 30,
+
+}
