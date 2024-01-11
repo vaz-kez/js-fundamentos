bf77948ffd7ea920d62338361bceff7f537ba5c7
Marcos Vazquez <marcosvazquez@mzkez.local>
2024-01-11 20:31:08 +0100

    Modified 04-booleans.js

--- 04-booleans.js
+++ 04-booleans.js
@@ -6,6 +6,8 @@ trueVar = false;
 console.log(trueVar);
 // False
 
+// Igual simple (=) asigna un valor a una variable
+
 console.log("Igual doble solo numbers: " + (5 == 5));
 console.log("Igual doble con string y number: " + ("5" == 5));
 // Igual doble: true
@@ -20,19 +22,19 @@ console.log("Es 2 mayor que 3? " + (2 > 3)); //False
 console.log("Es 3 mayor que 2? " + (3 > 2)); //True
 
 console.log("Es 3 mayor o igual que 3? " + (3 >= 3)); // True
-console.log("Es 3 mayor o igual que 3? " + (3 >= Number(3))); //True (Number(3) hace comparar la igualación triple)
+console.log("Es 3 mayor o igual que 3? " + (3 >= Number(3))); //True   (Number(3) se utiliza para convertir el posible string(3) a número.
 
 var isSameName = "juan" === "Juan";
 console.log("Son el mismo nombre? " + isSameName); //False
 
-var isSameName = "juan".toUpperCase() === "Juan".toUpperCase(); // Convierte los nombres a mayúsculas
+var isSameName = "juan".toUpperCase() === "Juan".toUpperCase(); // toUpperCase convierte los nombres a letra mayúscula y los hace iguales
 console.log("Son el mismo nombre? " + isSameName); //True
 
 //Truthy
 console.log("Es 2 truthy? " + Boolean(2)); // True
 console.log("Es PMBA truthy? " + Boolean("PMBA")); // True
 console.log("Es true truthy? " + Boolean(true)); // True
-// Boolean convierte el valor en true
+// Boolean comprueba que un dato tiene valor (binario, 0 - 1)
 
 //Falsy
 console.log("Es 0 falsy? " + Boolean(0)); // False  //El número 0 no representa valor
