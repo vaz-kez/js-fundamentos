21d6760a72b97e8fe40690ae1dc4fc5075d4c117
Marcos Vazquez <marcosvazquez@mzkez.home>
2024-01-15 21:34:41 +0100

    Created 09-loops-3.js

---
+++ 09-loops-3.js
@@ -0,0 +1,20 @@
+
+
+var i = 0;
+
+while (i <= 10) {
+  i++
+}
+console.log(`La variable i vale ${i}`);
+
+console.log(`--------------------------`)
+//////////////////////////////////////////////////////
+
+// El bucle 'do' no se utiliza mucho
+var j = 0;   //  Se toma la variable
+
+do {
+  j++;      //  Se cambia la cuenta en el punto que se quiera del bucle
+} while (j <= 10);    // Y se compara / termina el valor al final 
+
+console.log(`La variable J vale ${j}`)
