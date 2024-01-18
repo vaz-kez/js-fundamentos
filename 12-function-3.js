bebe1cbf738f62fe710c6ca1ca48a42bcd865e15
Marcos Vazquez <marcosvazquez@mzkez.home>
2024-01-18 19:45:32 +0100

    Scope and Shadowing

---
+++ 12-function-3.js
@@ -0,0 +1,53 @@
+
+var name = 'Marcos';
+console.log('1. name:', name);
+
+// Esta función agrega el cambio de valor a la variable 'name'
+function changeName(newName) {
+  name = newName;
+}
+
+// En este console.log no estamos invocando la función
+console.log('2. name:', name);
+
+/////////////////////////////////////
+changeName('Alberto');
+console.log('3. name: ', name);
+
+console.log('--------------------------------')
+
+///////////////////////////////////////////////////////////////////
+
+function sum(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function doOperation(operation, a, b) {
+  if (operation === 'sum') {
+    return sum(a, b);
+  } else if (operation === 'substract') {
+    return substract(a, b);
+  } else {
+    return 'No operation'
+  }
+}
+
+const result = doOperation('sum', 3, 4);
+
+console.log(`El resultado es: ${result}`);
+
+
+
+
+
+
+
+
+
+
+
+
