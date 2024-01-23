4b3ea673686bf428607859a05b246146b71819b9
Marcos Vazquez <marcosvazquez@mzkez.home>
2024-01-23 21:20:19 +0100

    Promises resolve/reject_then/catch

--- 18-async.js
+++ 18-async.js
@@ -1,37 +1,36 @@
-// // Promesas
+// // // // Promesas
 
 
-// const MyName = 'Marcos'
-// console.log(`Mi nombre es: ${MyName}`);
+const MyName = 'Marcos'
+console.log(`Mi nombre es: ${MyName}`);
 
-// /* 
-// Esta promesa se ejecutará al final del código
-// Lo que se ejecuta de esta promesa será la resolución o el fallo
-// La promesa tiene que ejecutar 'Resolve' o 'Reject'
-// 'Resolve' ... resuelve la promesa, la asigna as un valor y se trabajará con ese valor
-// 'Reject' .... hace que la promesa explote y que el código explote......pero podemos trabajar con ese ERROR.
-// */
-// const promise = new Promise((resolve, reject) => {
-//   console.log('Ejecutando promesa');
+/* 
+Esta promesa se ejecutará al final del código
+Lo que se ejecuta de esta promesa será la resolución o el fallo
+La promesa tiene que ejecutar 'Resolve' o 'Reject'
+'Resolve' ... resuelve la promesa, la asigna as un valor y se trabajará con ese valor
+'Reject' .... hace que la promesa explote y que el código explote......pero podemos trabajar con ese ERROR.
+*/
+const promise = new Promise((resolve, reject) => {
+  console.log('Ejecutando promesa');
 
-//   //resolve('Resuelta');
-//   reject('Promesa rota');
-// });
-// //Para resolve
-// //Termina la promesa
-// promise.then((value) => {
-//   console.log(`Promesa resuelta correctamente con el valor:`, value);;
-// });
+  //resolve ('Resuelta');
+  reject('Promesa rota');
+});
 
-// //Para reject
-// //Termina la promesa
-// promise.catch((error) => {
-//   console.log(error);
-// })
+//Para termina la promesa 'resolve'
+promise.then((value) => {
+  console.log(`Promesa resuelta correctamente con el valor:`, value);;
+});
 
-// console.log(promise);
+//Para terminar la promesa 'reject'
+promise.catch((error) => {
+  console.log(error);
+})
 
-// console.log('Este código se ejecuta antes que la promise');
+console.log(promise);
+
+console.log('Este código se ejecuta antes que la promise');
 
 ///////////////////////////////////////////////////////////////////
 
@@ -56,5 +55,6 @@ getAmountInBank()
   .catch((error) => {
     console.log(`Error: ${error}`);
   });
+
 //Siempre se debe concatenar 'then' y 'catch'
 //Y se pueden poner tantos 'then' como queramos
