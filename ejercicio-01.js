20cb41ab11ca105dcb6e53707442efcd4e0632f4
vaz-kez <146764232+vaz-kez@users.noreply.github.com>
2024-01-24 21:34:33 +0100

    Add files via upload

---
+++ ejercicio-01.js
@@ -0,0 +1,13 @@
+
+var myFavouriteHero = 'Hulk';
+
+var x = 50;
+
+var h = 5;
+
+var y = 10;
+
+var z = (h + y);
+
+console.log(`El resultado de sumar h + y es: ${z}`);
+//El resultado de sumar h + y es: 15
---
+++ ejercicio-02.js
@@ -0,0 +1,35 @@
+// 1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25. const character = {name: 'Jack Sparrow', age: 10};
+
+const name = 'Jack Sparrow';
+
+const character = { name: 'Jack Sparrow', age: 10 }
+
+function changeAge(newAge) {
+  age = newAge;
+}
+newAge = 25;
+console.log(`Soy el capitán ${name} , tengo ${newAge} años`);
+
+
+/* 1.2 Declara 3 variables con los nombres y valores siguientes firstName = 'Jon';
+lastName = 'Snow';
+age = 24;
+Muestralos por consola de esta forma:
+'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+*/
+
+const firstName = 'Jon';
+const lastName = 'Snow';
+const age = 24;
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
+
+/*
+1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de ambos juguetes.
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+*/
+
+const toy1 = { name: 'Buss myYear', price: 19 };
+const toy2 = { name: 'Rallo mcKing', price: 29 };
+
