9d9b5448f48047d57b3e826d32c5e697bc27a0be
Marcos Vazquez <marcosvazquez@mzkez.home>
2024-03-06 20:17:33 +0100

    JOIN

---
+++ 08-join.js
@@ -0,0 +1,16 @@
+// JOIN une los elementos de un array usando un separador
+
+const words = ['Hola', 'que', 'tal', '?'];
+
+const sentence = words.join(' ');  //  Hola que tal ?
+// const sentence = words.join('/n');  // Separa las palabras en lineas
+
+// let sentence = '';
+
+// for (let i = 0, i <  words.length; i++) {
+//   sentence += words[i];
+//   sentence += ' ';
+// }
+
+console.log(sentence);
+// Hola que tal ?
